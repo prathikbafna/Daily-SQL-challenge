d0b93af35e6530e29d93e80a9b1bab9221e1441d
prathikbafna <44114369+prathikbafna@users.noreply.github.com>
2025-09-20 22:19:41 -0600

    Sept 20 Daily Sql challenge

---
+++ SQL_09_20_2025.sql
@@ -0,0 +1,96 @@
+<--
+
+
+1098. Unpopular Books
+Solved
+Medium
+Topics
+SQL Schema
+Pandas Schema
+Table: Books
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| book_id        | int     |
+| name           | varchar |
+| available_from | date    |
++----------------+---------+
+book_id is the primary key (column with unique values) of this table.
+ 
+
+Table: Orders
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| order_id       | int     |
+| book_id        | int     |
+| quantity       | int     |
+| dispatch_date  | date    |
++----------------+---------+
+order_id is the primary key (column with unique values) of this table.
+book_id is a foreign key (reference column) to the Books table.
+ 
+
+Write a solution to report the books that have sold less than 10 copies in the last year, excluding books that have been available for less than one month from today. Assume today is 2019-06-23.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Books table:
++---------+--------------------+----------------+
+| book_id | name               | available_from |
++---------+--------------------+----------------+
+| 1       | "Kalila And Demna" | 2010-01-01     |
+| 2       | "28 Letters"       | 2012-05-12     |
+| 3       | "The Hobbit"       | 2019-06-10     |
+| 4       | "13 Reasons Why"   | 2019-06-01     |
+| 5       | "The Hunger Games" | 2008-09-21     |
++---------+--------------------+----------------+
+Orders table:
++----------+---------+----------+---------------+
+| order_id | book_id | quantity | dispatch_date |
++----------+---------+----------+---------------+
+| 1        | 1       | 2        | 2018-07-26    |
+| 2        | 1       | 1        | 2018-11-05    |
+| 3        | 3       | 8        | 2019-06-11    |
+| 4        | 4       | 6        | 2019-06-05    |
+| 5        | 4       | 5        | 2019-06-20    |
+| 6        | 5       | 9        | 2009-02-02    |
+| 7        | 5       | 8        | 2010-04-13    |
++----------+---------+----------+---------------+
+Output: 
++-----------+--------------------+
+| book_id   | name               |
++-----------+--------------------+
+| 1         | "Kalila And Demna" |
+| 2         | "28 Letters"       |
+| 5         | "The Hunger Games" |
++-----------+--------------------+
+
+-->
+
+
+-- Write your PostgreSQL query statement below
+
+
+with over_ten as (
+select book_id, SUM(quantity) from
+Orders 
+where dispatch_date >= DATE '2018-06-23' 
+GROUP BY book_id
+having SUM(quantity) >= 10 
+)
+
+SELECT book_id, name
+FROM 
+Books B
+where available_from <= DATE '2019-06-23' - INTERVAL '1' MONTH
+and book_id not in (select book_id from over_ten)
